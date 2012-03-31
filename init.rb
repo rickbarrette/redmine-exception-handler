fa75c83099d0bc2b73d4655ddbbb42c10b1834bf
Ricky Barrette <rickbarrette@gmail.com>
2012-03-31 01:09:22 -0400

    Increased verison code to 0.0.4

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_exception_handler do
   name 'Redmine Exception Handler plugin'
   author 'Rick Barrette <rickbarrette@gmail.com>'
   description 'An Exception Report Handler plugin for Redmine'
-  version '0.0.3'
+  version '0.0.4'
   url 'http://example.com/redmine/exceptionhandler'
   author_url 'http://rickbarrette.dyndns.org'
 end
