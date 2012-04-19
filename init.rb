3a37b3f0d1d85af4ad97d6e372ccaf635590715c
Ricky Barrette <rickbarrette@gmail.com>
2012-04-19 13:14:08 -0400

    Increased version code to 0.0.5
    also I updated the description

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ require 'redmine'
 Redmine::Plugin.register :redmine_exception_handler do
   name 'Redmine Exception Handler plugin'
   author 'Rick Barrette <rickbarrette@gmail.com>'
-  description 'An Exception Report Handler plugin for Redmine'
-  version '0.0.4'
+  description 'An Exception Report Handler plugin for Redmine <= 1.4.x'
+  version '0.0.5'
   url 'http://example.com/redmine/exceptionhandler'
   author_url 'http://rickbarrette.dyndns.org'
 end
